d3e6ddbcf156f05dd76774cf0ed5d13aa8ee1e12
Sanders76 <sanders76@list.ru>
2023-01-06 21:53:06 +0300

    Added code

---
+++ String_variant/Program.cs
@@ -0,0 +1,134 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив 
+// из строк длина которых меньше или либо равна трем символам.
+// Первоначальный массив можно ввести с клавиатуры либо задать на старте выполнения алгоритма.
+
+
+
+Console.WriteLine();
+
+Console.Write("Ведите количество элементов в массиве: ");
+
+int lengthOfFirstArray = InputInt();
+
+string[] array = new string[lengthOfFirstArray];
+
+string[] arrayFirst = FillArray(array);
+
+Console.WriteLine();
+
+Console.WriteLine("Введенный массив выглядит следующим образом:");
+
+PrintArray(arrayFirst);
+
+Console.WriteLine();
+
+Console.Write("Ведите ограничивающее значение для количества символов элементов массива: ");
+
+int limitingValue = InputInt();
+
+string[] desiredArray = FillingArrayWithGivenElementSize(arrayFirst, limitingValue);
+
+Console.WriteLine();
+
+if (desiredArray.Length == 0)
+{
+    Console.WriteLine("В новом массиве отсутствуют элементы.");
+}
+else
+{
+    Console.WriteLine("Новый массив, состоящий из элементов, удовлетворяющих заданному ограничению, выглядит следующим образом:");
+    PrintArray(desiredArray);
+}
+
+Console.WriteLine();
+
+
+
+// Метод заполнения массива с определенным размеров элементов
+
+string[] FillingArrayWithGivenElementSize(string[] arrayFirst, int limitingValue)
+{
+    // Находим какой будет длина искомого массива
+
+    int lengthOfSecondArray = 0;
+
+    string elementValue = string.Empty;
+
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        elementValue = arrayFirst[i];
+
+        if (elementValue.Length <= limitingValue)
+        {
+            lengthOfSecondArray++;
+        }
+    }
+
+    // Заполняем искомый массив элементами в соответствии с установленным ограничением
+
+    string[] arraySecond = new string[lengthOfSecondArray];
+
+    int iSecond = 0;
+
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        elementValue = arrayFirst[i];
+
+        if (elementValue.Length <= limitingValue)
+        {
+            arraySecond[iSecond] = arrayFirst[i];
+
+            iSecond++;
+        }
+    }
+    return arraySecond;
+}
+
+
+
+// Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
+
+
+
+//метод заполнения массива значениями, вводимыми через консоль
+
+string[] FillArray(string[] arr)
+{
+    Console.WriteLine();
+
+    Console.WriteLine("Введите символьные значения элементов массива:");
+
+    Console.WriteLine();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите значение {i + 1}-го элемента: ");
+
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
+
+//Метод печати массива
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[ {string.Join(", ", array)} ]");
+}
---
+++ Variant_with_numbers/Program.cs
@@ -0,0 +1,127 @@
+﻿// Вариант решения для массива из чисел
+
+
+Console.WriteLine();
+
+Console.Write("Ведите количество элементов в массиве: ");
+
+int lengthOfFirstArray = InputInt();
+
+int[] array = new int[lengthOfFirstArray];
+
+int[] arrayFirst = FillArray(array);
+
+Console.WriteLine();
+
+Console.WriteLine("Введенный массив выглядит следующим образом:");
+
+PrintArray(arrayFirst);
+
+Console.WriteLine();
+
+Console.Write("Ведите ограничивающее значение для элементов массива: ");
+
+int limitingValue = InputInt();
+
+int[] desiredArray = FillingArrayWithGivenElementSize(arrayFirst, limitingValue);
+
+Console.WriteLine();
+
+if (desiredArray.Length == 0)
+{
+    Console.WriteLine("В новом массиве отсутствуют элементы.");
+}
+else
+{
+    Console.WriteLine("Новый массив, состоящий из элементов, удовлетворяющих заданному ограничению, выглядит следующим образом:");
+    PrintArray(desiredArray);
+}
+
+Console.WriteLine();
+
+
+
+// Метод заполнения массива с определенным размеров элементов
+
+int[] FillingArrayWithGivenElementSize(int[] arrayFirst, int limitingValue)
+{
+    // Находим какой будет длина искомого массива
+    
+    int lengthOfSecondArray = 0;
+
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i] <= limitingValue)
+        {
+            lengthOfSecondArray++;
+        }
+    }
+
+    // Заполняем искомый массив элементами в соответствии с установленным ограничением
+
+    int[] arraySecond = new int[lengthOfSecondArray];
+
+    int iSecond = 0;
+
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i] <= limitingValue)
+        {
+            arraySecond[iSecond] = arrayFirst[i];
+
+            iSecond++;
+        }
+    }
+    return arraySecond;
+}
+
+
+
+// Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
+
+
+
+//метод заполнения массива числами, вводимыми через консоль
+
+int[] FillArray(int[] arr)
+{
+    Console.WriteLine();
+
+    Console.WriteLine("Введите значения элементов массива:");
+
+    Console.WriteLine();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите значение {i + 1}-го элемента: ");
+
+        int num = int.Parse(Console.ReadLine());
+
+        arr[i] = num;
+    }
+    return arr;
+}
+
+
+
+//Метод печати массива
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[ {string.Join(", ", array)} ]");
+}
